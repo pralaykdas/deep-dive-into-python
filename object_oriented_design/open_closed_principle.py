ef182151d5d7ad992dbe51b6b46d05d4e2bc0d97
Pralay Das <31288358+pralaykdas@users.noreply.github.com>
2025-05-06 14:51:33 +0530

    Added more details to OCP code.

--- object_oriented_design/open_closed_principle.py
+++ object_oriented_design/open_closed_principle.py
@@ -1,6 +1,14 @@
-# Open-Closed Principle
-# The principle states that the software entities like classes, methods, functions, etc. should be open for extensions, but closed for modification.
+# Open-Closed Principle (OCP)
+
+"""
+“Software entities (classes, modules, functions, etc.) should be open for extension but closed for modification.”
+
+# The principle states that the software entities should be open for extension, but closed for modification.
 # The purpose is to make it easier to add new features to the system, without directly modifying the existing code.
+# Purpose:
+    * Make the code easier to add new features to the system, without directly modifying the existing code.
+"""
+
 
 # Problem
 from math import pi
@@ -24,6 +32,23 @@ rectangle = Shape("rectangle", width=10, height=5)
 rectangle.calculate_area()
 circle = Shape("circle", radius=5)
 circle.calculate_area()
+
+"""
+Why it violates OCP?
+Adding a new shape like Square requires modifying the Shape class:
+
+-> Add new shape logic in __init__
+
+-> Add new case in calculate_area
+
+Every new shape adds more if/elif statements, which:
+
+-> Bloats the class
+
+-> Makes it more fragile (existing logic might break)
+
+-> Breaks OCP — you're modifying a stable class repeatedly
+"""
 
 
 # Solution
@@ -62,3 +87,12 @@ class Square(Shape):
 
     def calculate_area(self):
         return self.side**2
+
+"""
+How it follows OCP?
+-> You can add new shapes (like Triangle, Ellipse, etc.) by creating new subclasses — no need to touch existing Shape, Circle, or Rectangle classes.
+
+-> Each shape’s logic is encapsulated and self-contained.
+
+-> The base class Shape is closed for modification, but open for extension via inheritance.
+"""
